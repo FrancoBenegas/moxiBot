30f15e5732f9f8d7dc996842335fd81c422ca735
FrancoBenegas <francoextremo823@gmail.com>
2026-02-15 11:11:17 -0300

    fix fallos
    correccion botones comando nia y 1 comando para ver desde donde se esta ejecutando el bot

--- Eventos/Client/ready.js
+++ Eventos/Client/ready.js
@@ -162,10 +162,12 @@ module.exports = async (Moxi) => {
     const { getStartupComponentV2 } = require("../../Components/V2/startupEmbedComponent");
     const { MessageFlags } = require("discord.js");
     const channelId = process.env.ERROR_CHANNEL_ID || '1459913736050704485';
-    Moxi.channels.fetch(channelId).then(channel => {
-        if (channel && channel.isTextBased()) {
-            const component = getStartupComponentV2(Moxi);
-            channel.send({ content: '', components: [component], flags: MessageFlags.IsComponentsV2 }).catch(() => { });
-        }
-    });
+    Moxi.channels.fetch(channelId)
+        .then((channel) => {
+            if (channel && channel.isTextBased()) {
+                const component = getStartupComponentV2(Moxi);
+                channel.send({ content: '', components: [component], flags: MessageFlags.IsComponentsV2 }).catch(() => { });
+            }
+        })
+        .catch(() => null);
 };
